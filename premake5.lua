b879a6680b40391796ea15a30d4f78c1a3fa9926
FastIgnitionStudios <jlb@fastignitionstudios.com>
2025-05-25 11:17:55 +0100

    Removed test triangle and rectangle mesh, added 3d perspective and depth testing

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+project "FastGLTF"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
+
+    targetdir "../Binaries/%{cfg.buildcfg}"
+    objdir "../Intermediate/%{cfg.buildcfg}"
+
+    files {
+        "src/**.cpp",
+        "src/**.ixx",
+        "include/**.hpp",
+        "deps/simdjson.h"
+    }
+
+    includedirs {
+        "",
+        "include/",
+        "deps/",
+        
+    }
+
+
+    
+    filter "system:windows"
+        defines {"PLATFORM_WINDOWS"}
+    
+    filter "configurations:Debug"
+        symbols "on"
+    
+    
+    filter "configurations:Release"
+        optimize "on"
+
+    
+    filter "configurations:Dev"
+        optimize "on"
